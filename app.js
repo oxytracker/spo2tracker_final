d04c3e4aed68d4b163840ca5e877af2235f62668
oxytracker <oxytracker@gmail.com>
2022-06-03 13:20:23 +0530

    getdata

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const app=express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+"/index.html")
+app.get('/getdata',function(req,res){
+  console.log(req.query);
+  res.send(req.query.name);
 });
 
 app.listen(process.env.PORT || 5000);
